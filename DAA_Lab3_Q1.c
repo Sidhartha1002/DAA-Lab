c29e86216ef3bf09e049e122a797c4fb84b54768
Sidhartha1002 <134572048+Sidhartha1002@users.noreply.github.com>
2023-08-16 20:33:25 +0530

    Update DAA_Lab3_Q1.c

--- DAA_Lab3_Q1.c
+++ DAA_Lab3_Q1.c
@@ -1,49 +1,54 @@
 #include <stdio.h>
 
-int binary_search_leftmost(int arr[], int size, int key, int *comparisons) {
-    int left = 0, right = size - 1;
-    int index = -1;
-
+int binarySearchLeftmost(int arr[], int size, int key, int *comparisons) {
+    int left = 0;
+    int right = size - 1;
+    int result = -1;
+    
     while (left <= right) {
         int mid = left + (right - left) / 2;
+        
         (*comparisons)++;
-
+        
         if (arr[mid] == key) {
-            index = mid;
-            right = mid - 1;  // Move left to find leftmost appearance
+            result = mid;
+            right = mid - 1; // Search in the left half for leftmost appearance
         } else if (arr[mid] < key) {
             left = mid + 1;
         } else {
             right = mid - 1;
         }
     }
-
-    return index;
+    
+    return result;
 }
 
 int main() {
     int size, key;
+    
     printf("Enter size of array: ");
     scanf("%d", &size);
-
+    
     int arr[size];
+    
     printf("Enter elements of the array: ");
     for (int i = 0; i < size; i++) {
         scanf("%d", &arr[i]);
     }
-
+    
     printf("Enter the key to be searched: ");
     scanf("%d", &key);
-
+    
     int comparisons = 0;
-    int index = binary_search_leftmost(arr, size, key, &comparisons);
-
+    int index = binarySearchLeftmost(arr, size, key, &comparisons);
+    
     if (index != -1) {
         printf("%d found at index position %d\n", key, index);
         printf("Number of comparisons: %d\n", comparisons);
     } else {
         printf("%d not found in the array\n", key);
     }
-
+    
     return 0;
 }
+
